7142224749b58bda38bcdbdc607af67a3dd16323
inralmeida <inara.ralmeida@gmail.com>
2021-02-22 22:15:03 -0300

    add estrutura basica

---
+++ src/components/ExpirienceBar.tsx
@@ -0,0 +1,13 @@
+export function ExpirienceBar(){
+    return(
+        <header className='experienceBar'>
+            <span>0p xp</span>
+            <div>
+                <div style={{width: '50%'}} />
+
+                <span className='currentExperience' style={{left: "50%"}}> 300px </span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    )
+}
